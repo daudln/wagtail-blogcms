c423f4b1720186ddaebcab379b1182688c08e3fc
daudln <daudnamayala@gmail.com>
2024-02-13 10:31:07 +0300

    Initial

---
+++ blogcms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-m*g$os0i272ac=#kreu6x-)#x(j(a%fl!t9mqn&uja6iw6f!jd"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
